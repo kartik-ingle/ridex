5888394566369a9f5407e1bd15d92c5741279e10
kartik Ingle <ikartik1211@gmail.com>
2026-03-29 17:34:07 +0530

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,30 @@
+'use client'
 import React from 'react'
+import {motion} from 'motion/react'
 
 function Footer() {
   return (
-    <div>
-      
+    <div className='w-full bg-black text-white'>
+      <motion.div
+        initial = {{opacity: 0, y: 40}}
+        whileInView={{opacity: 1, y:0}}
+        transition = {{duration: 0.6, ease: "easeOut"}}
+        viewport={{once: true}}
+        className='max-w-7xl mx-auto px-6 py-16'
+      >
+
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-12'>
+          <div>
+            <h2 className='text-2xl font-bold tracking-wide'>RIDEX</h2>
+            <p className='mt-4 text-gray-400 text-sm leading-relaxed'>Book any vehicle - from bikes to trucks.
+               Trusted owners. Transport pricing.
+            </p>
+
+            
+          </div>
+        </div>
+
+      </motion.div>
     </div>
   )
 }
